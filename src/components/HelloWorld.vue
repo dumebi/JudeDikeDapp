3d58d7fe702a2dc0d223b8db7e34e54f2cd794c6
Jude Dike <dikejude49@gmail.com>
2019-06-20 16:42:45 +0100

    feat: - deployed Dapp

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,135 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Token Symbol</h3>
+      <p>{{token[0]}}</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Token Name</h3>
+      <p>{{token[1]}}</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Total Supply</h3>        
+      <p>{{parseInt(token[2])}}</p>
+    </div>
   </div>
+  <div class="row">
+    <div class="col-sm-12">
+      <h3>Minter</h3>
+      <p>{{minter}}</p>
+    </div>
+  </div>
+  <div class="row justify-content-center">
+    <div class="col-sm-6 ">
+      <h3>Mint token</h3>
+      <form>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Amount of tokens to mint</label>
+        <input v-model="amount" type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter token amount">
+      </div>
+      <button @click="mint" :disabled="amount < 1" type="button" class="btn btn-primary">Mint</button>
+    </form>
+    </div>
+  </div>
+  <!-- <div class="row justify-content-center">
+    <div class="col-sm-8 ">
+      <h3>Set Minter</h3>
+      <form>
+      <div class="form-group">
+        <label for="exampleInputEmail1">New minter address</label>
+        <input v-model="newMinter" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+      </div>
+      <button @click="setMinter" :disabled="newMinter < 1" type="button" class="btn btn-primary">Set Minter</button>
+    </form>
+    </div>
+  </div> -->
+</div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data: function () {
+    return {
+      token: [],
+      minter: '',
+      newMinter: '',
+      amount: '',
+      notificationSystem: {
+        options: {
+          ballon: {
+            position: 'topRight'
+          },
+          info: {
+            position: 'topRight'
+          },
+          success: {
+            position: 'topRight'
+          },
+          warning: {
+            position: 'topRight'
+          },
+          error: {
+            position: 'topRight'
+          }
+        }
+      }
+    }
+  },
+  mounted () {
+    this.init()
+  },
+  methods: {
+    async init() {
+      // console.log(window.MyContract)
+      try {
+        const tokenDetails = await window.MyContract.methods.details().call()
+        this.token = tokenDetails
+
+        const minterDetails = await window.MyContract.methods.minter().call()
+        this.minter = minterDetails
+        this.$toast.success('Token details fetched', '', this.notificationSystem.options.success)
+      } catch (error) {
+        this.$toast.error('Error getting token details', '', this.notificationSystem.options.error)
+      }
+     
+    },
+
+    async mint() {
+      // console.log(this.amount, this.minter)
+      try {
+        const minter = await window.MyContract.methods.mint(this.amount).send({
+            from: this.minter,
+            gas: 2000000
+        })
+        if(minter){
+          this.$toast.success('Token has been minted', '', this.notificationSystem.options.success)
+        }
+        const tokenDetails = await window.MyContract.methods.details().call()
+
+        this.token = tokenDetails
+      } catch (error) {
+        this.$toast.error('could not mint token', '', this.notificationSystem.options.error)
+      }
+     
+    },
+
+    // async setMinter() {
+    //   console.log(this.amount, this.minter)
+    //   try {
+    //     const minter = await window.MyContract.methods.setMinter(this.newMinter).send({
+    //         from: '0x77598660059c39924d068940B26E9F3fc373261A',
+    //         gas: 2000000
+    //     })
+    //     console.log(minter)
+    //     const minterDetails = await window.MyContract.methods.minter().call()
+    //     this.minter = minterDetails
+    //   } catch (error) {
+    //     console.log(error)
+    //   }
+     
+    // }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,476 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Web3 from 'web3'
+// Import component
+import VueLoading from 'vue-loading-overlay'
+import VueIziToast from 'vue-izitoast'
+// Import stylesheet
+import 'vue-loading-overlay/dist/vue-loading.css';
+import 'izitoast/dist/css/iziToast.min.css'
+
+Vue.use(VueLoading)
+Vue.use(VueIziToast)
+Vue.component('Loading', VueLoading)
 
 Vue.config.productionTip = false
+
+window.addEventListener('load', async function () {
+    if (window.ethereum) {
+      window.web3 = new Web3(ethereum);
+      try {
+          // Request account access if needed
+          await ethereum.enable();
+          // // Acccounts now exposed
+          // web3.eth.sendTransaction({/* ... */});
+      } catch (error) {
+          // User denied account access...
+      }
+  }
+  if (typeof web3 !== 'undefined') {
+    console.log('Web3 injected browser: OK.')
+    window.web3 = new Web3(window.web3.currentProvider)
+  } else {
+    console.log('Web3 injected browser: Fail. You should consider trying MetaMask.')
+    this.$toast.warning('Web3 injected browser: Fail. You should consider trying MetaMask.', '', this.notificationSystem.options.warning)
+    
+    // Below uses the Web3 HD Wallet to connect to ropsten
+    // -- this help me communicate with my smart contract since kaleido doesnt work atm
+    // var httpProvider = new Web3.providers.HttpProvider('http://kovan.infura.io/v3/942f12ce424f49ff8b130dc063febb48')
+    // var provider = new Web3HDWalletProvider("priority camera link lucky cave rug federal shiver canoe elegant student illegal", httpProvider);
+    // window.web3 = new Web3(provider)
+  }
+
+  this.console.log(ethereum.selectedAddress)
+
+  // window.ethereum.enable();
+
+  window.MyContract = new web3.eth.Contract([
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_volume",
+          "type": "uint256"
+        }
+      ],
+      "name": "addTotalSupply",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_spender",
+          "type": "address"
+        },
+        {
+          "name": "_value",
+          "type": "uint256"
+        }
+      ],
+      "name": "approve",
+      "outputs": [
+        {
+          "name": "success",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_value",
+          "type": "uint256"
+        }
+      ],
+      "name": "mint",
+      "outputs": [
+        {
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "renounceOwnership",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_newMinter",
+          "type": "address"
+        }
+      ],
+      "name": "setMinter",
+      "outputs": [
+        {
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_to",
+          "type": "address"
+        },
+        {
+          "name": "_value",
+          "type": "uint256"
+        }
+      ],
+      "name": "transfer",
+      "outputs": [
+        {
+          "name": "success",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_from",
+          "type": "address"
+        },
+        {
+          "name": "_to",
+          "type": "address"
+        },
+        {
+          "name": "_value",
+          "type": "uint256"
+        }
+      ],
+      "name": "transferFrom",
+      "outputs": [
+        {
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "transferOwnership",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "name": "_symbol",
+          "type": "string"
+        },
+        {
+          "name": "_name",
+          "type": "string"
+        },
+        {
+          "name": "_supply",
+          "type": "uint256"
+        }
+      ],
+      "payable": true,
+      "stateMutability": "payable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "name": "previousOwner",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "name": "newOwner",
+          "type": "address"
+        }
+      ],
+      "name": "OwnershipTransferred",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "name": "_from",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "name": "_to",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "name": "_value",
+          "type": "uint256"
+        }
+      ],
+      "name": "Transfer",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "name": "_owner",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "name": "_spender",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "name": "_value",
+          "type": "uint256"
+        }
+      ],
+      "name": "Approval",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "Mint",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "name": "minter",
+          "type": "address"
+        }
+      ],
+      "name": "Minter",
+      "type": "event"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "_totalSupply",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "_owner",
+          "type": "address"
+        },
+        {
+          "name": "_spender",
+          "type": "address"
+        }
+      ],
+      "name": "allowance",
+      "outputs": [
+        {
+          "name": "remaining",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "_owner",
+          "type": "address"
+        }
+      ],
+      "name": "balanceOf",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "decimals",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "details",
+      "outputs": [
+        {
+          "name": "",
+          "type": "string"
+        },
+        {
+          "name": "",
+          "type": "string"
+        },
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "isOwner",
+      "outputs": [
+        {
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "minter",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "name",
+      "outputs": [
+        {
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "owner",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "totalSupply",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ], '0x9872af6ec086938132878d1a62b83ca5bbeaa59f', {
+    from: '0x77598660059c39924d068940B26E9F3fc373261A', // default from address
+    gasPrice: '20000000000' // default gas price in wei, 20 gwei in this case
+});
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+})
